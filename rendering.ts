34863f3e532ede6238f2679f4c8a42a9df768c52
dovesadumbass-dot <dovesadumbass@gmail.com>
2025-12-16 01:07:25 -0600

    Update rendering.ts

--- rendering.ts
+++ rendering.ts
@@ -928,12 +928,13 @@ function populateEnergyReset(energy_reset_div: HTMLElement) {
     energy_reset_div.innerHTML = "";
 
     if (GAMESTATE.is_in_energy_reset) {
+        doEnergyReset();
         energy_reset_div.innerHTML = "<h2>Out of Energy</h2>" +
             "<p>You used up all your Energy, but this is not the end.</p>" +
             "<p>You keep half your Items (rounded up).</p>" +
             "<p>The effects of used Items disappear.</p>" +
-            "<p>You keep all your Skills and Perks.</p>";
-        doEnergyReset();
+            "<p>You should be mad.</p>";
+        
     } else {
         energy_reset_div.innerHTML = "<h2>Last Run</h2>";
     }
